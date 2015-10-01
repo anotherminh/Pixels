5d12f0782eb4c068bb8596054836ad0ebb13b341
minhngocln <minhngoc.ln@gmail.com>
2015-09-30 22:06:54 -0700

    add draw-drag functionality

--- js/canvas-view.js
+++ js/canvas-view.js
@@ -1,29 +1,47 @@
 (function () {
-  if (typeof window.Canvas === 'undefined') {
-    window.Canvas = {};
+  if (typeof window.Pixels === 'undefined') {
+    window.Pixels = {};
   }
 
-  View = Canvas.View = function (canvasObj, $canvas, $palette) {
+  View = Pixels.View = function (canvasObj, $canvas, $palette) {
     this.canvasObj = canvasObj;
     this.$canvas = $canvas;
     this.$palette = $palette;
     this.selectedColor = "red";
     this.setupCanvas();
+    this.setupPalette();
 
     this.bindEvents();
   };
 
+  View.PALETTE = [
+      'red', 'cornflowerblue', 'forestgreen', 'wheat', '#ccc', 'white',
+      'black', 'pink', 'purple'
+    ];
+
   View.prototype.bindEvents = function () {
+    var mousedown = false;
     var that = this;
     that.$palette.on("click", ".swatch", (function (event) {
          var $selectedColor = $(event.currentTarget);
-         var selectedClass = $selectedColor.attr('class');
-         that.selectedColor = selectedClass.split(" ")[0];
+         that.selectedColor = $selectedColor.data('color');
        }).bind(that));
 
-    that.$canvas.on("click", ".cell", (function (event) {
-      var $selectedCell = $(event.currentTarget);
-      $selectedCell.css('background', that.selectedColor);
+    that.$canvas.on("mousedown", ".cell", (function (event) {
+        mousedown = true;
+        var $selectedCell = $(event.currentTarget);
+        $selectedCell.css('background', that.selectedColor);
+      }).bind(that));
+
+    that.$canvas.on("mouseup", ".cell", (function (event) {
+      mousedown = false;
+    }).bind(that));
+
+    that.$canvas.on("mouseover", ".cell", (function (event) {
+      if (mousedown) {
+        var $selectedCell = $(event.currentTarget);
+        $selectedCell.css('background', that.selectedColor);
+      }
     }).bind(that));
   };
 
@@ -42,6 +60,20 @@
       $cell = that.$canvas.append("<div class='cell'></div>");
       $cell.data('idx', cell.idx);
       $cell.css('background', cell.color);
+    });
+  };
+
+  View.prototype.setupPalette = function () {
+    var that = this;
+    var width = that.canvasObj.width;
+
+    $('.palette').css('width', (width + ((width/10) * 2)).toString());
+
+    View.PALETTE.forEach (function (swatch) {
+      $swatch = $("<li class='swatch'></li>");
+      $swatch.data('color', swatch);
+      $swatch.css('background', swatch);
+      that.$palette.append($swatch);
     });
   };
 })();
--- palette.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  if (typeof window.Pixels === 'undefined') {
-    window.Pixels = {};
-  }
-
-  Palette = Pixels.Palette = function () {
-    this.colors = ['red', 'cornflowerblue', 'forestgreen'];
-  };
-})();
